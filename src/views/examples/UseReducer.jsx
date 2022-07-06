c02858695233a6602067899f45a2cea049b53279
Paulo <paulomafra2004@gmail.com>
2022-07-06 10:17:31 -0300

    useReducer chalenge

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
@@ -13,14 +13,25 @@ function reducer(state, action){
     switch(action.type){
         case 'number_Add2':
             return {...state, number: state.number + 2}
+        case 'number_mult7':
+            return {...state, number: state.number * 7}
+        case 'number_div25':
+            return {...state, number: state.number / 25}
+        case 'number_AddN':
+        return {...state, number: state.number + parseInt(action.payload.numN)}
         case 'login':
-            return {...state, user: {name: action.payload}}
+            return {...state, user: {name: action.payload.nome}}
+        case 'integer':
+            return {...state, number: parseInt(state.number)}
+        case 'limpar':
+            return {...state, user: null, number: 0}
         default:
             return state
     }
 }
 
 const UseReducer = (props) => {
+    const [inputNumber, setInputNumber] = useState(0)
     const [state, dispatch] = useReducer(reducer, initialState)
 
     return (
@@ -29,16 +40,21 @@ const UseReducer = (props) => {
                 title="Hook UseReducer"
                 subtitle="Uma outra forma de ter estado em componentes funcionais!"
             />
-            <SectionTitle title="Exercício"/>
+            <SectionTitle title="Exercício + Desafio"/>
             <div className="center">
-                {state.user ?
-                    <span className="text">{state.user.name}</span> :
-                    <span className="text">Sem usuário</span>
-                }
+                <span className="text">{state.user ? state.user.name : 'Sem usuário'}</span>
                 <span className="text">{state.number}</span>
                 <div>
-                    <button className="btn" onClick={() => dispatch({type: 'login', payload: 'Maria'})}>Login</button>
+                    <button className="btn" onClick={() => dispatch({type: 'limpar'})}>Limpar</button>
+                    <button className="btn" onClick={() => dispatch({type: 'login', payload: {nome: 'Maria'}})}>Login</button>
+                    <button className="btn" onClick={() => dispatch({type: 'integer'})}>Int</button>
                     <button className="btn" onClick={() => dispatch({type: 'number_Add2'})}>+2</button>
+                    <button className="btn" onClick={() => dispatch({type: 'number_mult7'})}>×7</button>
+                    <button className="btn" onClick={() => dispatch({type: 'number_div25'})}>÷25</button>
+                </div>
+                <div>
+                    <input type="number" className="input" value={inputNumber} onChange={e => setInputNumber(e.target.value)}/>
+                    <button className="btn" onClick={() => dispatch({type: 'number_AddN', payload: {numN: inputNumber}})}>Add</button>
                 </div>
             </div>
         </div>
