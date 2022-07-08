f391190ef2e78187ef0ee3a16f12405a1971f2bd
Paulo <paulomafra2004@gmail.com>
2022-07-07 23:01:31 -0300

    Custom Hooks II

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 import {useCounter} from '../../hooks/useCounter'
+import {useFecth} from '../../hooks/useFetch'
 
 const UseRef = (props) => {
     const [count, inc, dec] = useCounter()
+    const url = "http://files.cod3r.com.br/curso-react/estados.json"
+    const response = useFecth(url)
+
+    function showStates(states){
+        return states.map(states => <li key={states.nome}>{states.nome} - {states.sigla}</li>)
+    }
 
     return (
         <div className="UseCustom">
@@ -19,6 +26,12 @@ const UseRef = (props) => {
                     <button className="btn" onClick={() => dec()}>-1</button>
                     <button className="btn" onClick={() => inc()}>+1</button>
                 </div>
+            </div>
+            <SectionTitle title="Exercício #02"/>
+            <div className="center">
+                <ul>
+                    {!response.loading ? showStates(response.data) : false}
+                </ul>
             </div>
         </div>
     )
